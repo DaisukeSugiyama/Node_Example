d688b88535134de1c8351b6927f3b625bc74aa4d
杉山大輔 <rubber.d.s.soul@gmail.com>
2017-10-26 13:41:08 +0900

    leveldb lebel-dbtest.js テスト中

--- node_leveldb/lebel-dbtest.js
+++ node_leveldb/lebel-dbtest.js
@@ -1,73 +1,148 @@
-//leveldb
-var levelup = require('level');
-var opt = { valueEncoding: 'json' }
+ // leveldb
+ var levelup = require('level');
+ var opt = { valueEncoding: 'json' };
+ var db = levelup('./testdb2', opt);
 
 
-var db = levelup('./testdb2', opt);
-db.batch();
+ var ops = [
+     { type: 'del', key: 'father' },
+     { type: 'put', key: 'name', value: 'Yuri Irsenovich Kim' },
+     { type: 'put', key: 'dob', value: '16 February 1941' },
+     { type: 'put', key: 'spouse', value: 'Kim Young-sook' },
+     { type: 'put', key: 'occupation', value: 'Clown' }
+ ]
 
-//値を設定
-db.put('Mikan', 'Orange', function(err) {
-    if (err) {
-        console.log('Err', err);
-        return;
-    }
-    testGet();
-});
+ db.batch(ops, function(err) {
+         if (err) return console.log('Ooops!', err)
+         console.log('Great success dear leader!')
+         testkeys();
+     })
+     /*
+      db.batch()
+          .put('fruits!apple', {
+              name: 'Apple',
+              price: 300,
+              color: 'red'
+          })
+          .put('fruits!orange', {
+              name: 'Orange',
+              price: 180,
+              color: 'orange'
+          })
+          .put('fruits!banana', {
+              name: 'Banana',
+              price: 200,
+              color: 'yellow'
+          })
+          .put('fruits!kiwi', {
+              name: 'Kiwi',
+              price: 220,
+              color: 'green'
+          })
+          .put('snack!poteto', {
+              name: 'Poteto-Snack',
+              price: 340,
+              color: 'brown'
+          })
+          .put('snack!choco', {
+              name: 'Choco-Snack',
+              price: 220,
+              color: 'black'
+          })
+          .write(testkeys);
+     */
 
-//値を取得
-function testGet() {
-    db.get('Mikan', function(err, value) {
-        if (err) {
-            console.log('Error', err);
-            return;
-        }
-        console.log('Mikan=' + value);
-        testBatch();
-    });
-}
+ /*値を設定
+ db.put('Mikan', 'Orange', function(err) {
+     if (err) {
+         console.log('Err', err);
+         return;
+     }
+     testGet();
+ });
+ */
 
-//一括設定
-function testBatch() {
-    db.batch()
-        .put('Ruby', 'pink')
-        .put('Dia', 'Red')
-        .put('pearl', 'white')
-        .write(function() {
-            testGet2();
-        });
-}
+ /*値を取得
 
-//値を取得2
-function testGet2() {
-    db.get('Dia', function(err, value) {
-        console.log('Dia=' + value);
-        testkeys();
-    })
-}
+ function testGet() {
+     db.get('Mikan', function(err, value) {
+         if (err) {
+             console.log('Error', err);
+             return;
+         }
+         console.log('Mikan=' + value);
+         testBatch();
+     });
+ }
 
-//キーの一覧を取得
-function testkeys() {
-    console.log("keys");
-    db.createKeyStream()
-        .on('data', function(key) {
-            console.log(" - " + key);
-        })
-        .on('end', testKeyValues);
-}
+ //一括設定
 
-//キーと値の一覧を取得する
-function testKeyValues() {
-    console.log("\nkey-value-list:");
-    /*
-    db.createKeyStream()
-        .on('data', function(data) {
-            var key = data.key;
-            var o = data.value;
-            console.log("+ key = " + data.key);
-            console.log("| color = " + o.color);
-            console.log("|price = " + o.price);
-        })
-        .on('end')
-        */
-}
+ function testBatch() {
+     db.batch()
+         .put('Ruby', 'pink')
+         .put('Dia', 'Red')
+         .put('pearl', 'white')
+         .write(function() {
+             testGet2();
+         });
+ }
+*/
+
+ /*値を取得2
+ function testGet2() {
+     db.get('Dia', function(err, value) {
+         console.log('Dia=' + value);
+         testkeys();
+     })
+ }
+ */
+
+ //キーの一覧を取得
+
+ function testkeys() {
+     console.log("keys");
+     /* db.get('fruits!banana', function(value) {
+          console.log(value)
+      });
+      */
+
+     db.createKeyStream()
+         .on('data', function(data) {
+             console.log(" - " + data);
+         })
+         .on('end', function() {
+             //console.log("end");
+         });
+
+ }
+
+ //キーと値の一覧を取得する
+
+ function testKeyValues() {
+     console.log("\nkey-value-list:");
+
+     db.createKeyStream()
+         .on('data', function(data) {
+             var key = data.key;
+             var o = data.value;
+             console.log("+ key = " + data.key);
+             console.log("| color = " + o.color);
+             console.log("|price = " + o.price);
+         })
+         .on('end', testSearch)
+ }
+
+ function testSearch() {
+     console.log('\nrange-search:');
+     var opt = {
+         start: "fruits!",
+         end: "fruits!\xFF"
+     };
+     db.createReadStream(opt)
+         .on('data', function(data) {
+             console.log("+ key=" + data.key);
+         })
+         .on('end', function() {
+             console.log('ok')
+         });
+ }
